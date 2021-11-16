8e6d89ee54d615ea3d319578a4792eae72bc2e11
Kaushik Sawant <kaushik.sawant@ugamsolutions.com>
2021-11-16 22:46:35 +0530

    servlet added

---
+++ core/src/main/java/com/ugamsol/core/servlets/calculator.java
@@ -0,0 +1,26 @@
+package com.ugamsol.core.servlets;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.servlets.SlingAllMethodsServlet;
+import org.apache.sling.servlets.annotations.SlingServletPaths;
+import org.osgi.service.component.annotations.Component;
+
+import javax.servlet.Servlet;
+import java.io.IOException;
+
+@Component(service = Servlet.class)
+@SlingServletPaths(
+        value = {"/bin/calc"}
+)
+public class calculator extends SlingAllMethodsServlet {
+
+    @Override
+    protected void doGet(final SlingHttpServletRequest req, final SlingHttpServletResponse resp) throws IOException, IOException {
+        int firstNumber = Integer.parseInt(req.getParameter("firstNumber"));
+        int secondNumber = Integer.parseInt(req.getParameter("secondNumber"));
+        int answer = firstNumber+secondNumber;
+        resp.getWriter().print(answer);
+    }
+
+}
