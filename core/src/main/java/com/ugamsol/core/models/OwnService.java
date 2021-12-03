7780cb40084aa2542365d7478d88bc2abb909e79
Kaushik Sawant <kaushik.sawant@ugamsolutions.com>
2021-12-03 17:34:09 +0530

    added

---
+++ core/src/main/java/com/ugamsol/core/models/OwnService.java
@@ -0,0 +1,8 @@
+package com.ugamsol.core.models;
+
+public interface OwnService {
+    public String getServiceName();
+    public int getServiceCount();
+    public boolean isLiveData();
+    public String getRunModes();
+}
---
+++ core/src/main/java/com/ugamsol/core/models/OwnServiceModel.java
@@ -0,0 +1,10 @@
+package com.ugamsol.core.models;
+
+
+public interface OwnServiceModel {
+    public String getServiceName();
+    public int getServiceCount();
+    public boolean isLiveData();
+    public String getRunModes();
+
+}
