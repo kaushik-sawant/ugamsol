6672af92caf9a43cf93b874707a2292b507a4adf
kaushik-sawant <85493576+kaushik-sawant@users.noreply.github.com>
2021-12-07 17:27:29 +0530

    Delete OwnService.java

--- core/src/main/java/com/ugamsol/core/models/OwnService.java
+++
@@ -1,8 +0,0 @@
-package com.ugamsol.core.models;
-
-public interface OwnService {
-    public String getServiceName();
-    public int getServiceCount();
-    public boolean isLiveData();
-    public String getRunModes();
-}
